67b97d46bf016d5d1d444474e8908a8a2e47c96f
Albert Eftimie <albertozaurusrex012@gmail.com>
2022-11-28 19:27:30 +0200

    builder dar nu stiu cum sa i dau doar lui add

---
+++ src/basics_oop/ch2/Actor.java
@@ -0,0 +1,15 @@
+package basics_oop.ch2;
+
+public class Actor {
+
+    private String nume;
+    private int varsta;
+    private int Premiu[];
+
+    public Actor()
+    {
+
+    }
+
+
+}
---
+++ src/patterns/builder/Main.java
@@ -0,0 +1,19 @@
+package patterns.builder;
+
+public class Main {
+    public static void main(String[] args) {
+        //Computer computer=new Computer("16GB", true, "1TB", "RTX2060", "17", "Dolby");
+        Computer.Builder builder =new Computer.Builder("16GB");
+        builder.setAudio("AudioDevice");
+        builder.setAudio("AudioDevice").setBluetooth(true).setCPU("17").setGPU("RTX").setStorage("1TB");
+
+        Computer computer = builder.build();
+
+
+        System.out.println("Computer :" + computer);
+
+
+    }
+
+
+}
