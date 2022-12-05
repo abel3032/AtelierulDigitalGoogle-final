830c0236256276e7926fd68235f1bcdf4b281f30
Albert Eftimie <albertozaurusrex012@gmail.com>
2022-12-05 19:47:00 +0200

    iterator

---
+++ src/patterns/ch1/ArrayCustomIterator.java
@@ -0,0 +1,31 @@
+package patterns.ch1;
+
+public class ArrayCustomIterator {
+
+    private int[] array;
+    private int position;
+
+    public ArrayCustomIterator(int[] array)
+    {
+        this.array=array;
+        this.position=0;
+    }
+
+
+    public int next()
+    {
+        if(hasNext()){
+            return array[this.position++];
+        }
+        else{
+            return 0;
+        }
+    }
+
+    public boolean hasNext()
+    {
+        return (position< array.length);
+    }
+
+
+}
