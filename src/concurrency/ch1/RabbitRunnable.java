e68bdeb5677915d772a7c1d9ebf1ac3516d48ade
Albert Eftimie <albertozaurusrex012@gmail.com>
2022-12-19 20:07:27 +0200

    ch1

---
+++ src/concurrency/ch1/RabbitRunnable.java
@@ -0,0 +1,14 @@
+package concurrency.ch1;
+
+public class RabbitRunnable implements Runnable{
+    private int nr;
+
+    public RabbitRunnable(int nr) {
+        this.nr = nr;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Rabbit " + this.nr + " is running");
+    }
+}
---
+++ src/concurrency/ch1/RabbitThread.java
@@ -0,0 +1,13 @@
+package concurrency.ch1;
+
+public class RabbitThread extends Thread{
+    private int nr;
+
+    public RabbitThread(int nr) {
+        this.nr = nr;
+    }
+
+    public void run() {
+        System.out.println("Rabbit " + this.nr + " is running");
+    }
+}
