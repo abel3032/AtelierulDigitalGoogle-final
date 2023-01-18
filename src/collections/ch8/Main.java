f4a02c44131332ec364386bd05529c6a0c57ac39
Albert Eftimie <albertozaurusrex012@gmail.com>
2023-01-18 13:50:26 +0200

    ch8

---
+++ src/collections/ch8/Main.java
@@ -0,0 +1,19 @@
+package collections.ch8;
+
+import java.util.*;
+
+public class Main {
+    public static void useArrayList() {
+        List list = new ArrayList<String>();
+        list.add("Prim");
+        list.add("Secund");
+        list.add("Tert");
+        list.add("Gol");
+        list.add("Orice");
+        System.out.println(list);
+    }
+
+    public static void main(String[] args) {
+        useArrayList();
+    }
+}
